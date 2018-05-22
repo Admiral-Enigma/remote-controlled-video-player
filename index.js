377f6c8d959a25c868f1713e303ded053e5f2cf9
Alexander <MrRandomgamerdkHD@gmail.com>
2018-05-22 11:05:43 +0200

    Dis code sucks

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(8080);
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/fisksildfisk', function (req, res) {
+  res.sendFile(__dirname + '/control.html');
+});
+
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  //console.log(Object.keys(io.sockets.sockets).length);
+  socket.on('getClients', function () {
+    socket.emit('clients', Object.keys(io.sockets.sockets).length)
+  })
+
+  socket.on('startVideo', function () {
+    console.log('started video');
+    socket.emit('startTheVideo')
+  })
+
+  socket.on('started', function () {
+    console.log('video started');
+  })
+
   socket.on('my other event', function (data) {
     console.log(data);
   });
